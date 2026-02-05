0253a28aab70a4da4036495f29bf2da34893a7f3
Gabriel Hoppe <gabrielhoppe@MacBook-Air-de-Gabriel.local>
2026-02-05 04:06:17 -0300

    initial commit

---
+++ src/main/java/solo/sr4s_stats/Sr4sStatsApplication.java
@@ -0,0 +1,13 @@
+package solo.sr4s_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sr4sStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sr4sStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/solo/sr4s_stats/Sr4sStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package solo.sr4s_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sr4sStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
